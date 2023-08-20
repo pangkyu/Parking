465068a04981ffb986339b9eca8bff35476f61ef
pangkyu <baesense@naver.com>
2023-08-20 15:21:21 +0900

    Feat : 라우팅 추가

--- parking/components/Header.tsx
+++ parking/components/Header.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex flex-col items-center justify-between mt-[10px]">
+    <header className="flex flex-col items-center justify-between mt-[10px]">
       <div className="flex max-w-5xl w-[70%] items-center justify-between ">
         <Link href="/">
           <Image src={Title} alt="parking" className="w-[40px]" />
         </Link>
-        <p>근처 주차장찾기</p>
+        <Link href="/parking">
+          <p>근처 주차장찾기</p>
+        </Link>
       </div>
-    </div>
+    </header>
   );
 }
