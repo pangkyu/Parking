eee15133f9c9fae0c9e46399c1ee5fc3e4c900da
pangkyu <baesense@naver.com>
2023-08-21 17:59:09 +0900

    Feat : 주차장 데이터 받아오기
    Fix me : 현재 1000개만 받아올 수 있는데 전체 데이터를 받아올 수 있도록 수정 필요

--- parking/app/parking/page.tsx
+++ parking/app/parking/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-
 import { parseString } from "xml2js";
 
 export default function parking() {
   const [data, setData] = useState(null);
+  let parkingCount = 0;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}0/999/`
+        );
         if (response.ok) {
           const responseData = await response.text();
           parseString(responseData, (err, result) => {
@@ -28,6 +30,37 @@ export default function parking() {
     fetchData();
   }, []);
   console.log(data);
-  return <p>adsf</p>;
-  // return <div>{data ? <p>{JSON.stringify(data)}</p> : <p>loading</p>}</div>;
+  if (data) {
+    parkingCount = Number(data.GetParkingInfo.list_total_count[0]);
+  }
+
+  return (
+    <>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>주차장 이름</th>
+            <th>주차장 주소</th>
+            <th>주차장 형태</th>
+            <th>유/무료</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            ? data.GetParkingInfo.row.map((item, index: number) => {
+                console.log(item);
+                return (
+                  <div key={index}>
+                    <th>{item.PARKING_NAME}</th>
+                    <th>{item.ADDR}</th>
+                    <th>{item.PARKING_TYPE_NM}</th>
+                    <th>{item.PAY_NM}</th>
+                  </div>
+                );
+              })
+            : null}
+        </tbody>
+      </table>
+    </>
+  );
 }
