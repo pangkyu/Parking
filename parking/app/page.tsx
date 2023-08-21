5ca2da7bd6b2dfba9110590f1b643de941a56bef
pangkyu <baesense@naver.com>
2023-08-21 15:23:24 +0900

    Feat : open api 데이터 가져오기

--- parking/app/page.tsx
+++ parking/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex max-w-5xl w-full items-center justify-between ">
-        <p>asdf</p>
+    <div className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex max-w-5xl w-[70%] items-center justify-between ">
+        <p>페이지지</p>
         <p>adssdss</p>
         <p>adssdss</p>
         <p>adssdss</p>
       </div>
-    </main>
+    </div>
   );
 }
--- parking/app/parking/page.tsx
+++ parking/app/parking/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+import { parseString } from "xml2js";
+
 export default function parking() {
-  return <p>테스트 test 123123</p>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+        if (response.ok) {
+          const responseData = await response.text();
+          parseString(responseData, (err, result) => {
+            if (err) {
+              console.error("ERROR Parsing XML : ", err);
+            } else {
+              setData(result);
+            }
+          });
+        }
+      } catch (err) {
+        console.error("ERROR Fetching data: ", err);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data);
+  return <p>adsf</p>;
+  // return <div>{data ? <p>{JSON.stringify(data)}</p> : <p>loading</p>}</div>;
 }
