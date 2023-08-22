d135a21325cad2b81d1570fdd33aa31f99882162
pangkyu <baesense@naver.com>
2023-08-22 21:03:51 +0900

    Design : 모바일 뷰에 맞게 디자인 수정

--- parking/app/parking/page.tsx
+++ parking/app/parking/page.tsx
@@ -55,51 +55,51 @@ export default function parking() {
     fetchData();
   }, []);
 
-  console.log("data", data);
-
   return (
-    <main className="flex items-center justify-center">
-      <table className=" table-auto border-collapse border border-slate-400">
-        <thead>
-          <tr>
-            <th className="border border-slate-300">주차장 이름</th>
-            <th className="border border-slate-300">주소</th>
-            <th className="border border-slate-300">형태</th>
-            <th className="border border-slate-300">유/무료</th>
-            <th className="border border-slate-300">요금</th>
-            <th className="border border-slate-300">총 주차대수</th>
-            <th className="border border-slate-300">주차 가능대수</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data
-            ? data.map((item: any, index: number) => {
-                return (
-                  <tr key={index}>
-                    <td className="border border-slate-300">
-                      {item.PARKING_NAME}
-                    </td>
-                    <td className="border border-slate-300">{item.ADDR}</td>
-                    <td className="border border-slate-300">
-                      {item.PARKING_TYPE_NM}
-                    </td>
-                    <td className="border border-slate-300">{item.PAY_NM}</td>
-                    <td className="border border-slate-300">
-                      {item.RATES}원, {item.TIME_RATE}분
-                    </td>
+    <main className="text-gray-600 body-font w-[100%] flex justify-center items-center">
+      <div className=" flex text-center mx-auto">
+        <table className="table-auto border-collapse border border-slate-400 text-[8px] lg:text-[12px]">
+          <thead>
+            <tr>
+              <th className="border border-slate-300">주차장 이름</th>
+              <th className="border border-slate-300">주소</th>
+              {/* <th className="border border-slate-300">형태</th> */}
+              <th className="border border-slate-300">유/무료</th>
+              <th className="border border-slate-300">요금</th>
+              <th className="border border-slate-300">총 주차대수</th>
+              <th className="border border-slate-300">주차 가능대수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              ? data.map((item: any, index: number) => {
+                  return (
+                    <tr key={index}>
+                      <td className="border border-slate-300">
+                        {item.PARKING_NAME}
+                      </td>
+                      <td className="border border-slate-300">{item.ADDR}</td>
+                      {/* <td className="border border-slate-300">
+                          {item.PARKING_TYPE_NM}
+                        </td> */}
+                      <td className="border border-slate-300">{item.PAY_NM}</td>
+                      <td className="border border-slate-300">
+                        {item.RATES}원, {item.TIME_RATE}분
+                      </td>
 
-                    <td className="border border-slate-300">
-                      {item.CAPACITY}대
-                    </td>
-                    <td className="border border-slate-300">
-                      {item.CAPACITY - item.CUR_PARKING}대
-                    </td>
-                  </tr>
-                );
-              })
-            : null}
-        </tbody>
-      </table>
+                      <td className="border border-slate-300">
+                        {item.CAPACITY}대
+                      </td>
+                      <td className="border border-slate-300">
+                        {item.CAPACITY - item.CUR_PARKING}대
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
