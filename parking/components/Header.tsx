018440659b3e723dc1a64efd52a5804a04c1b0c0
pangkyu <baesense@naver.com>
2023-08-22 19:59:01 +0900

    Design : Header 컴포넌트 css 모바일 뷰로 전환

--- parking/components/Header.tsx
+++ parking/components/Header.tsx
@@ -4,14 +4,24 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-between mt-[10px]">
-      <div className="flex max-w-5xl w-[70%] items-center justify-between ">
-        <Link href="/">
-          <Image src={Title} alt="parking" className="w-[40px]" />
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link
+          href="/"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          <Image
+            src={Title}
+            alt="parkingGood"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+          />
+          <span className="ml-3 text-xl">Parking Good</span>
         </Link>
-        <Link href="/parking">
-          <p>근처 주차장찾기</p>
-        </Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link href="/parking" className="mr-5 hover:text-gray-900">
+            근처 주차장 찾기
+          </Link>
+        </nav>
       </div>
     </header>
   );
