cb7b0570729174ab0abc3a3dc5ca788f2a0bca25
pangkyu <baesense@naver.com>
2023-08-25 03:09:34 +0900

    Test : Vercel & local 변수 설정 후 배포 테스트
    https://github.com/pangkyu/Parking/issues/1#issuecomment-1692003583

--- parking/app/parktest/page.tsx
+++ parking/app/parktest/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function ParkTest() {
   const [test, setTest] = useState([]);
   const fetchData = async () => {
-    const res = await fetch("https://parking-good.vercel.app/api/dataList");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dataList`);
     const data = await res.json();
     setTest(data);
   };
