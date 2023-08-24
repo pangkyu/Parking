62638a3f89f91d1268202e735bed130dce55f48a
pangkyu <baesense@naver.com>
2023-08-25 01:38:42 +0900

    Test : 에러 해결 테스트 2
    https://github.com/pangkyu/Parking/issues/1

--- parking/app/parktest/page.tsx
+++ parking/app/parktest/page.tsx
@@ -5,9 +5,7 @@ import { useState } from "react";
 export default function ParkTest() {
   const [test, setTest] = useState([]);
   const fetchData = async () => {
-    const res = await fetch(
-      "https://parking-good.vercel.app/parktest/api/dataList"
-    );
+    const res = await fetch("https://parking-good.vercel.app/api/dataList");
     const data = await res.json();
     setTest(data);
   };
