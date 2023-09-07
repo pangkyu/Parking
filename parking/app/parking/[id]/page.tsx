f9c9e31eddf099de0757bd5ae6fb64bf96ab263f
pangkyu <baesense@naver.com>
2023-09-07 14:45:24 +0900

    Comment : todo 내용 추가
    디테일 페이지 접속 시, 파킹 코드를 파라미터로 넘겨서 해당 데이터를 가져오려했으나, 파킹 코드로 관리되지가 않아서 다른 방법으로 데이터 get하는 방법 찾아야한다.

--- parking/app/parking/[id]/page.tsx
+++ parking/app/parking/[id]/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+/**
+ * @todo
+ * 파킹 코드로 검색이 되는 줄 알았는데 안됩니다
+ * 수정 필요 !
+ */
+
+import { useParams } from "next/navigation";
 
 export default function detail() {
-  const router = useRouter();
+  const param = useParams();
+  console.log(param.id);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`api/parkingList/${param.id}/${param.id}`);
+      console.log(response.text());
+    } catch (err) {
+      console.error("Detail API ERROR : ", err);
+    }
+  };
+  fetchData();
+
   return (
     <>
       <p>디테일</p>
