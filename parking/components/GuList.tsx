ff68fe45dc5102fb2e26abdd7bc7c576486cb7e3
pangkyu <baesense@naver.com>
2023-09-24 22:42:05 +0900

    Design : 구 리스트 디자인 수정

--- parking/components/GuList.tsx
+++ parking/components/GuList.tsx
@@ -5,13 +5,12 @@ export default function GuList() {
     <>
       {Object.values(SEOUL_GU).map((value, index: number) => {
         return (
-          <button
+          <div
             key={index}
-            type="button"
-            className="flex  text-white bg-indigo-500 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-900 text-[10px] w-[80px] lg:text-[13px] lg:w-[100px] justify-center"
+            className="flex text-white bg-indigo-500 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-900 text-[10px] w-[80px] lg:text-[13px] lg:w-[100px] justify-center"
           >
             {value}
-          </button>
+          </div>
         );
       })}
     </>
